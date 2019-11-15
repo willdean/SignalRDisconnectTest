b761bd257e301f70628fa75e237679d142bf8ca0
willdean <will.dean@indcomp.co.uk>
2019-11-15 11:57:49 +0000

    Initial Add

---
+++ SignalR/TestController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
+
+namespace SignalRTest.SignalR
+{
+    public class TestController : Controller
+    {
+        readonly IHubContext<TestHub, ITestHub> _hubContext;
+
+        public TestController(IHubContext<TestHub, ITestHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Good()
+        {
+            _hubContext.Clients.All.good(new Random().Next(100));
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Bad()
+        {
+            var json = "{ 'a' : 'b' }";
+            object message = JsonConvert.DeserializeObject(json);
+            _hubContext.Clients.All.bad(message);
+            return Ok();
+        }
+    }
+}
---
+++ SignalR/TestHub.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRTest.SignalR
+{
+    public interface ITestHub
+    {
+        Task good(int count);
+        Task bad(object obj);
+    }
+
+    public class TestHub : Hub<ITestHub>
+    {
+
+    }
+}
